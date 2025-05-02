1c9a15e9d111fa4f7f43cbd3c3dff1366863b35d
nskrus054@gmail.com <nskrus054@gmail.com>
2025-05-02 09:53:31 +0700

    Update readme and search_config to unnessesary shanges

--- search_config.sh
+++ search_config.sh
@@ -8,5 +8,10 @@ fi
 file=$1
 word=$2
 
+if [ ! -f "$file" ]; then
+    echo "Файл не найден: $file"
+    exit 1
+fi
+
 echo "Результаты поиска '$word' в файле '$file':"
 grep -i "$word" "$file" || echo "Совпадений не найдено."
